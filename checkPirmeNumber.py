15eed5f520fca106471feb0490809580b7e6c0ce
Pawan-ky <59448263+Pawan-ky@users.noreply.github.com>
2023-02-02 12:57:22 +0000

    is prime number

--- checkPirmeNumber.py
+++ checkPirmeNumber.py
@@ -1,11 +1,6 @@
 """
 A prime number is a natural number that is only divisible by 1 and by itself.
 
-it is enough if we traverse upto the root of a number. Whenever we find a divisor, 
-we print it and also print the quotient. If the quotient is same, we ignore it. 
-Because, root of a perfect square will give same quotient as itself.
-quotients are the numbers that are on the other side of the root.
-So, its okay if we stop traversing at root.
 """
 
 import math
